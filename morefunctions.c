cf9b29c8bf4ded7acbc355d977c09462ebd3400f
Muita0808 <magdalinemuita@gmail.com>
2023-09-19 14:28:48 -0700

    morefunctions u,x,X

--- morefunctions.c
+++ morefunctions.c
@@ -18,19 +18,6 @@ int _printf(const char *format, ...)
 			i++; /* Move past '%' */
 			switch (format[i])
 			{
-				case 'd': /* Handle integer specifier */
-				case 'i':
-					{
-						int int_arg = va_arg(args, int);
-						num_chars += printf("%d", int_arg);
-					}
-					break;
-				case 'b': /* Handle binary specifier */
-					{
-						unsigned int uint_arg = va_arg(args, unsigned int);
-						num_chars += print_binary(uint_arg);
-					}
-					break;
 				case 'u': /* Handle unsigned decimal specifier */
 					{
 						unsigned int uint_arg = va_arg(args, unsigned int);
@@ -70,42 +57,4 @@ int _printf(const char *format, ...)
 	}
 	va_end(args);
 	return (num_chars);
-}
-
-int print_binary(unsigned int num)
-{
-	int num_chars = 0;
-	int binary[32]; // Assume a 32-bit integer
-	
-	/* Convert the integer to binary and store it in the binary array*/
-	for (int i = 31; i >= 0; i--)
-	{
-		binary[i] = num % 2;
-		num /= 2;
-	}
-	
-	/* Print the binary representation*/
-	for (int i = 0; i < 32; i++)
-	{
-		num_chars += putchar('0' + binary[i]);
-	}
-	return num_chars;
-}
-
-int main(void)
-{
-	int num = 4;
-	unsigned int binary_num = 433;
-	unsigned int decimal_num = 1.23;
-	unsigned int octal_num = 6657853;
-	unsigned int hex_num = 256542095;
-	
-	_printf("This is a number: %d\n", num);
-	_printf("Binary representation: %b\n", binary_num);
-	_printf("Unsigned decimal: %u\n", decimal_num);
-	_printf("Octal representation: %o\n", octal_num);
-	_printf("Lowercase hexadecimal: %x\n", hex_num);
-	_printf("Uppercase hexadecimal: %X\n", hex_num);
-	
-	return( 0);
-}
+}Z
